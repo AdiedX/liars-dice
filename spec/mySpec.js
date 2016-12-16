6ba060473c0fbcdabfe28723e354c85408bab834
Adi Sivasankaran <adi.sivasankaran@gmail.com>
2016-12-16 03:04:07 -0600

    Update unit tests

--- spec/mySpec.js
+++ spec/mySpec.js
@@ -6,15 +6,16 @@ var randomGenerator = seedRandom();
 
 describe('Liars Dice', function() {
 	var game, randomIndex, playerDiceVal, playerValueCount;
-	beforeEach(function() {
+
+	var init = function() {
 		game = new LiarsDice(4);
 		game.init();
 		randomIndex = Math.floor(randomGenerator() * 5);
 		playerDiceVal = game.getPlayerDiceValues(1)[randomIndex];
 		playerValueCount = game.getPlayerValueCount(1, playerDiceVal);
+	};
 
-		spyOn(console, 'log');
-	});
+	init();
 
 	it('should generate random, numeric, dice values for players', function() {
 		var players = game._players;
@@ -35,38 +36,34 @@ describe('Liars Dice', function() {
 		expect(flag).toBe(true);
 	});
 
-	it('should have a move() method which initiates a move, keeps track of all moves, and logs to the console', function() {
+	it('should have a move() method which keeps track of all moves, current dice value-count, and logs a message', function() {
 		expect(game._currentValCount).toBe(0);
 		expect(game._moves.length).toBe(0);
-
+		spyOn(game, '_logMessage');
 		game.move(1, 1, playerDiceVal);
-
 		expect(game._currentValCount).not.toBe(0);
 		expect(game._moves.length).toBe(1);
-
-		expect(console.log).toHaveBeenCalled();
+		expect(game._logMessage).toHaveBeenCalled();
 	});
 
 	it('should have a claim() method which calculates probability and logs to the user a statement', function() {
-		expect(game._claims.length).toBe(0);
-
-		spyOn(game, '_probability');
+		spyOn(game, '_trackClaims');
+		spyOn(game, '_logMessage');
+		spyOn(game, '_calculateCumulativeProbability');
+		spyOn(game, '_analyzeCumulativeProbability');
 		game.claim(3, playerDiceVal);
-
-		expect(game._probability).toHaveBeenCalled();
-		expect(game._claims.length).toBe(1);
-		expect(console.log).toHaveBeenCalled();
+		expect(game._calculateCumulativeProbability).toHaveBeenCalled();
+		expect(game._analyzeCumulativeProbability).toHaveBeenCalled();
+		expect(game._logMessage).toHaveBeenCalled();
+		expect(game._trackClaims).toHaveBeenCalled();
 	});
 
 	xit('should have a challenge() method which verifies if the last claim is true', function() {
+		spyOn(game, '_calculateTotalDiceCount');
+		spyOn(game, '_logMessage');
 		game.challenge();
-
-		var c = 0;
-		game._diceValues.forEach(function(playerValues) {
-			c += playerValues.length;
-		});
-
-		expect(game._diceCount).toBe(c);
+		expect(game._calculateTotalDiceCount).toHaveBeenCalled();
+		expect(game._logMessage).toHaveBeenCalled();
 	});
 
 });
